13cce8baa266fc52b984f9b809a25b08d015e208
Anna-Gladush <ann.gladush@gmail.com>
2025-12-15 18:32:51 +0200

    Finished 9th task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, stop, step = 1) {    
+    let result;
+    let range;
+    if (!Number.isInteger(start) || !Number.isInteger(stop) || start < 0 || stop < 0) {
+        return "ERROR"
+    }
 
-};
+    if (start <= stop) {
+        range = Array.from({ length: ((stop - start) / step) + 1 },(_, i) => start + i * step);
+    } else {
+        range = Array.from({ length: ((start - stop) / step) + 1 },(_, i) => stop + i * step);
+    }
+    result = range.reduce((total, current) => total + current, 0);
+    return result;
+}
+//     if (start > stop) {
+//         let rangeReverse = range.reverse();
+//         result = rangeReverse.reduce((total, current) => total + current, 0);
+//         return result;
+//     } else {
+//         result = range.reduce((total, current) => total + current, 0);
+//         return result;
+//     }
+// };
 
 // Do not edit below this line
 module.exports = sumAll;

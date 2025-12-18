f043b0f527119bd7bc46a3a5b95a0a1641021bc6
Anna-Gladush <ann.gladush@gmail.com>
2025-12-18 17:25:51 +0200

    done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr === undefined || arr.length === 0) {return 0;}
+	return arr.reduce((total, current) => total + current);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  if (a === 0 || a === 1) {return 1}
+	for (let i = 1; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

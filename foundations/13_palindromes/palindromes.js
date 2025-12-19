b2a2def734227a061b488a91d63c197ad08136dd
Anna-Gladush <ann.gladush@gmail.com>
2025-12-19 16:39:50 +0200

    Finished palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let letters = s
+                    .toLowerCase()
+                    .split('')
+                    .filter((letter) => (!letter.includes(" ") && !letter.includes(",") && !letter.includes(".") && !letter.includes("!") && !letter.includes("?") && !letter.includes(":") && !letter.includes(";")))
+                    .join('');
 
-};
+    let reverse = s
+                    .toLowerCase()
+                    .split('')
+                    .reverse()
+                    .filter((letter) => (!letter.includes(" ") && !letter.includes(",") && !letter.includes(".") && !letter.includes("!") && !letter.includes("?") && !letter.includes(":") && !letter.includes(";")))
+                    .join('');
+    if (letters === reverse) {
+        return true
+    } else {
+        return false};
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;

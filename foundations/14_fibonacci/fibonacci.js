c6d275ec4aa80956153715069f51f512f3f78bed
Anna-Gladush <ann.gladush@gmail.com>
2025-12-19 17:37:03 +0200

    Finished Fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(placeNumber) {
+    if (placeNumber === 0 || placeNumber === "0") {
+        return 0;
+    } else if (placeNumber === 1 || placeNumber === "1") {
+        return 1;
+    } else if (placeNumber < 0) {
+        return "OOPS"
+    }
 
+    let results = [1, 1];
+    const index = placeNumber - 1;
+    for (let i = 2; i < placeNumber; i++) {
+        const next = results[i - 1] + results[i-2];
+        results.push(next)
+    }
+    return results[index];
 };
 
 // Do not edit below this line

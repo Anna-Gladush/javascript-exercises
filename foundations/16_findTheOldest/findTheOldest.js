640bac20fe4a7de4147fe401b87478218bf0a360
Anna-Gladush <ann.gladush@gmail.com>
2025-12-19 18:56:18 +0200

    finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    const getAge = (person) => {
+        const deathYear = person.yearOfDeath ?? currentYear;
+        return deathYear - person.yearOfBirth;
+    }
+    const ordered = people.sort((a, b) => {
+        const lastPerson = getAge(a);
+        const nextPerson = getAge(b);
+        return lastPerson > nextPerson ? -1 : 1})
+    return ordered[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
